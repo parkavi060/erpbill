3c827762adc94c1ac15cec06c75c4ed091204961
boominathan2355 <boominathanlagirisamy@gmail.com>
2026-04-12 12:33:32 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/erpbill/',
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    mode === 'development' && vueDevTools(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
